4c19a28c98bfa0de3b97dbaeb8458e967b08803f
Dagmar Tichá <135602317+dadulkaticha@users.noreply.github.com>
2024-01-28 15:06:21 +0100

    Add files via upload

---
+++ PYTHON/Zkouska/main.py
@@ -0,0 +1,63 @@
+from kniha import Book 
+from knihovna import Library 
+
+def main(): 
+
+    book = Book("9;Animal Farm;Orwell George;Available") 
+
+    print(book) 
+
+    print() 
+
+
+    library = Library("data.txt") 
+
+    print(library) 
+
+    print() 
+
+ 
+    library.add_book(book) 
+
+    print() 
+
+ 
+    book.set_id(library.get_unique_id()) 
+
+    library.add_book(book) 
+
+    print() 
+
+ 
+    print("Dostupné knihy:") 
+
+    library.show_available_books() 
+
+    print() 
+
+ 
+    print("Půjčení knihy:") 
+
+    library.find_book_and_borrow_it("Kill") 
+
+    print() 
+
+    library.find_book_and_borrow_it("a") 
+
+    print() 
+
+    library.find_book_and_borrow_it("Great") 
+
+    print() 
+
+ 
+    print("Dostupné knihy:") 
+
+    library.show_available_books() 
+
+    print() 
+
+ 
+if __name__ == "__main__": 
+
+    main()
